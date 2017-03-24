ed0b49b8603ee555064646431bb8e9fb55abfa7e
Michel Valin <mfvalin@gmail.com>
2017-03-24 11:41:05 -0400

    force consistency of [rs].ssmuse.dot with r.load.dot

--- etc/profile.d/shortcut-tools_1.0_all.sh
+++ etc/profile.d/shortcut-tools_1.0_all.sh
@@ -1,5 +1,7 @@
 [[ -n "$MODULEPATH" ]] && export MODULEPATH="`eval echo $MODULEPATH`"
 export EC_SHORTCUT_PATH=${SSMUSE_BASE:-${SSM_DOMAIN_BASE}}
+s.use r.load.dot as s.ssmuse.dot
+s.use r.load.dot as r.ssmuse.dot
 [[ -n $BASH_VERSION ]] || { echo "WARNING: this is not a bash shell, auto completion not available" ; return ; }
 
 #make_fix_the_paths
